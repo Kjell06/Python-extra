07ec71a056958f9b70b9a1e3c84cb762e1f2aede
Kjell <34952@ma-web.nl>
2022-11-10 11:26:56 +0100

    update

---
+++ 03-Meme/maak_meme.py
@@ -0,0 +1,34 @@
+from PIL import Image, ImageFont, ImageDraw
+
+achtergrond = Image.open("sus.png")
+
+# Afmetingen opslaan in eigen variabelen
+breedte = achtergrond.width
+hoogte = achtergrond.height 
+
+lettertype = ImageFont.truetype("impact.ttf", 150)
+
+# Vraag aan de ImageDraw module om een tekengebied te maken op de achtergrond afbeelding
+tekengebied = ImageDraw.Draw(achtergrond)
+
+
+# Tekst schrijven
+tekst = "What are you doing"
+
+# Bereken de breedte en hoogte van de tekst
+tekst_breedte, tekst_hoogte = tekengebied.textsize(tekst, font=lettertype) 
+print("tekstbreedte=" + str(tekst_breedte) + ", tekst_hoogte=" + str(tekst_hoogte))
+
+# Tekst positie berekenen
+tekst_x = (breedte - tekst_breedte) / 2
+tekst_y = (hoogte - tekst_hoogte) / 2   
+
+tekengebied.multiline_text((tekst_x+33, tekst_y-400), tekst, font=lettertype, fill=(255,255,255))
+
+# Het resultaat tonen
+achtergrond.show()
+
+# En opslaan onder een andere naam
+achtergrond.save("meme_met_tekst.jpg")
+
+
---
+++ Discord bot/discord_bot.py
@@ -0,0 +1,4 @@
+
+filepointer=open("C:\Users\kjell\OneDrive\Ma\Bewijzenmap\Periode 1.1\Python extra\Discord bot", "r")
+bottoken=filepointer.readline()
+filepointer.close()
